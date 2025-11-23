6fbe346ace4cdbed0b398d3398096b38950a93a6
ArchitJaiswal001 <149718632+ArchitJaiswal001@users.noreply.github.com>
2025-11-24 03:02:10 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+The "Password Generator App" Debugging & Enhancement Challenge
+Welcome! Your job is to fix a buggy and incomplete Password Generator Application.
+A previous developer left this project partially implemented. The UI is inconsistent, some features don’t work properly, and key functionalities are missing.
+Your mission is to:
+
+
+Identify and fix the bugs.
+
+
+Add the missing functionalities.
+
+
+
+HOW TO START
+
+
+Open the project folder.
+
+
+The app contains:
+index.html
+style.css
+script.js
+
+
+Open index.html in any browser to run the password generator.
+
+
+
+WHAT YOU HAVE (EXISTING CODE)
+Basic Password Generation Logic
+
+
+Generates a random password
+
+
+Includes lowercase, uppercase, numbers, and symbols
+
+
+Basic UI layout implemented
+
+
+Copy-to-clipboard button partially implemented
+
+
+
+PROJECT STRUCTURE
+/password-generator
+│── index.html
+│── style.css
+│── script.js
+│── README.md
+│── issues.md
+
+YOUR GOAL
+Fix the issues and enhance the project with missing features.
+Make the password generator fully functional, secure, and user-friendly.
+
+
+Good luck debugging!
+Happy Coding ^_^
---
+++ script.js
@@ -0,0 +1,48 @@
+const passwordField = document.getElementById("password");
+const copyBtn = document.getElementById("copyBtn");
+const lengthInput = document.getElementById("length");
+
+const lowerCheck = document.getElementById("lowercase");
+const upperCheck = document.getElementById("uppercase");
+const numberCheck = document.getElementById("numbers");
+const symbolCheck = document.getElementById("symbols");
+
+const generateBtn = document.getElementById("generateBtn");
+
+function generatePassword() {
+    const length = parseInt(lengthInput.value);
+
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbol = "!@#$%^&*()_+{}[]<>?/";
+
+    let allowedChars = "";
+    let password = "";
+
+    if (lowerCheck.checked) allowedChars += lowercase;
+    if (upperCheck.checked) allowedChars += uppercase;
+    if (numberCheck.checked) allowedChars += numbers;
+    if (symbolCheck.checked) allowedChars += symbols;
+    if (!allowedChars) return "";
+
+
+    if (allowedChars === "") {
+        alert("Please select at least one option!");
+        return;
+    }
+
+    for (let i = 0; i <= length; i++) {
+        Math.random() * allowedChars.length
+        password += allowedChars[randomIndex];
+    }
+
+    passwordField.value = password;
+}
+
+generateBtn.addEventListener("click", generatePassword);
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(passwordField.value);
+    alert("Password Copied!");
+});
